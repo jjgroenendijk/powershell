a1d6fd2142ea5d652d9b6af6db4d5cf0ebe63d4d
jjgroenendijk <3110270+jjgroenendijk@users.noreply.github.com>
2023-04-18 14:20:29 +0200

    Fix unicode formatting

--- intune-win32-packaging.ps1
+++ intune-win32-packaging.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 
     Quick concept for automating a single step of Intune Packaging
 
@@ -12,9 +12,27 @@ if (!(Test-Path $workingDirectory))
 {
     # Dit kan wel korter
     New-Item -ItemType Directory -Path $workingDirectory
-    New-Item -ItemType Directory -Path $workingDirectory -Name "in"    New-Item -ItemType Directory -Path $workingDirectory -Name "out"    New-Item -ItemType Directory -Path $workingDirectory -Name "tmp"
+    New-Item -ItemType Directory -Path $workingDirectory -Name "in"
+    New-Item -ItemType Directory -Path $workingDirectory -Name "out"
+    New-Item -ItemType Directory -Path $workingDirectory -Name "tmp"
 }
 
 # Controleer of git geïnstalleerd is. Installeer indien nodig met WinGet
 if (!(git))
-{    Write-Output "Installing Git first"    winget install git.git}# Clone de git repository van MicrosoftStart-Process -FilePath "git" -ArgumentList "clone https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool.git ""$workingDirectory""" # Haal alle setups op$setups = Get-ChildItem -Path "$workingDirectory\in"Write-Output $setups# Package alle setups naar een intune formaatForeach ($setup in $setups){    Start-Process -FilePath "$workingDirectory\IntuneWinAppUtil.exe" -ArgumentList "-c ""$workingDirectory\in"" -s ""$setup"" -o ""$workingDirectory\out""" -WindowStyle Hidden}
+{
+    Write-Output "Installing Git first"
+    winget install git.git
+}
+
+# Clone de git repository van Microsoft
+Start-Process -FilePath "git" -ArgumentList "clone https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool.git ""$workingDirectory""" 
+
+# Haal alle setups op
+$setups = Get-ChildItem -Path "$workingDirectory\in"
+Write-Output $setups
+
+# Package alle setups naar een intune formaat
+Foreach ($setup in $setups)
+{
+    Start-Process -FilePath "$workingDirectory\IntuneWinAppUtil.exe" -ArgumentList "-c ""$workingDirectory\in"" -s ""$setup"" -o ""$workingDirectory\out""" -WindowStyle Hidden
+}
