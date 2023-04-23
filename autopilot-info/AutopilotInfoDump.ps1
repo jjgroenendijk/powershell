5f559df9a3b1e06bd9d261f80fcd2dc16ceb5dc0
jjgroenendijk <3110270+jjgroenendijk@users.noreply.github.com>
2023-04-23 16:39:38 +0200

    added offline autpilot HWID capturing

---
+++ autopilot-info/AutopilotInfoDump.ps1
@@ -0,0 +1,12 @@
+﻿Function OK-Continue {
+    $Shell = New-Object -ComObject "WScript.Shell"
+    $Button = $Shell.Popup("Autopilot info captured!", 0, "Finished", 0)
+}
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+Install-PackageProvider -Name "NuGet" -Force
+Install-Script -Name Get-WindowsAutopilotInfo -Force
+Get-WindowsAutopilotInfo -OutputFile "$scriptPath\AutopilotDevices.csv" -Append
+
+OK-Continue
---
+++ win32-packaging/intune-win32-packaging.ps1
@@ -0,0 +1,38 @@
+<#
+
+    Quick concept for automating a single step of Intune Packaging
+
+#>
+
+# Define working directory.
+$workingDirectory = "$env:USERPROFILE\Desktop\Application Prep"
+
+# Create working directory if it doesn't exist
+if (!(Test-Path $workingDirectory)) 
+{
+    # Dit kan wel korter
+    New-Item -ItemType Directory -Path $workingDirectory
+    New-Item -ItemType Directory -Path $workingDirectory -Name "in"
+    New-Item -ItemType Directory -Path $workingDirectory -Name "out"
+    New-Item -ItemType Directory -Path $workingDirectory -Name "tmp"
+}
+
+# Controleer of git geinstalleerd is. Installeer indien nodig met WinGet
+if (!(git))
+{
+    Write-Output "Installing Git first"
+    winget install git.git
+}
+
+# Clone de git repository van Microsoft
+Start-Process -FilePath "git" -ArgumentList "clone https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool.git ""$workingDirectory""" 
+
+# Haal alle setups op
+$setups = Get-ChildItem -Path "$workingDirectory\in"
+Write-Output $setups
+
+# Package alle setups naar een intune formaat
+Foreach ($setup in $setups)
+{
+    Start-Process -FilePath "$workingDirectory\IntuneWinAppUtil.exe" -ArgumentList "-c ""$workingDirectory\in"" -s ""$setup"" -o ""$workingDirectory\out""" -WindowStyle Hidden
+}
